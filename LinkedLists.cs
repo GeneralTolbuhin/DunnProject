f0934db220ec127fd15868b26a68f5f74bf533c3
GeneralTolbuhin <115687098+GeneralTolbuhin@users.noreply.github.com>
2023-03-28 00:02:48 +0300

    Update LinkedLists.cs

--- LinkedLists.cs
+++ LinkedLists.cs
@@ -1,6 +1,47 @@
-//First we creade a class for our 
+using System;
 
-class node
+public class LinkedLiast
 {
-  int data;
-  struct node*next;
+    public static void LinkedLST(string[] args)
+    {
+        
+    }
+}
+
+
+//A linked list is created inside a special linked list class
+
+
+// For a normal linked list we create the following class:
+
+private class Node(
+    private int data;  //we initialise the data of the linked list
+    private Node next;   //we make the first node of the list
+    public Node(int a) { 
+        data = a;
+        next = null;  //we set it to null
+    }  
+}  
+
+  
+ 
+private class LinkedList{
+  private node head; //we initialise the linked list with a node and a head
+  
+//For a doubly linked list
+
+private class DoubleNode {  
+    private int data;  
+    private DoubleNode prev;  //we create a pointer pointing to the previous value
+    private DoubleNode next;  //we create a pointer pointing to the next value
+    public DoubleNode(int a) {  
+        data = a;  
+        prev = null;  //we set both values to null
+        next = null;  
+    }  
+}  
+  
+  
+private class DoubleLinkedList {  
+    private DoubleNode head;  //we initialise the linked 
+}  

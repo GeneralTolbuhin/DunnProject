5310210d243cc56841d018ea7bc1e62cfc507586
Snozm <89019256+Snozm@users.noreply.github.com>
2023-03-28 05:03:42 +0300

    Update LinkedLists.py

--- LinkedLists.py
+++ LinkedLists.py
@@ -1,17 +1,25 @@
-#A linked list is created using "Node" classes
+#A linked list is created inside a special linked list class
 
 
-#For a singly linked list we create the following class:
+#For a singly linked list we create the following classes:
 
 public class Node{
   public int data;  #we initialise the data of the linked list
   public Node next;   #we make the first node of the list
+  
   public Node(int a){
     data = a;
     next = null;  #we set it to null
   }
 }
 
+public class LinkedList{
+  public Node head;  #we initialise the linked list with a node for a head
+  
+  public LinkedList(int a){
+    head = new Node(a);
+  }
+}
 
 #For a doubly linked list
 
@@ -19,9 +27,18 @@ public class DoubleNode{
   public int data;
   public DoubleNode prev;  #we create a pointer pointing to the previous value
   public DoubleNode next;  #we create a pointer pointing to the next value
+  
   public DoubleNode(int a){
     data = a;
     prev = null;  #we set both values to null
     next = null;
+  }
+}
+
+public class DoubleLinkedList{
+  public DoubleNode head;  #we initialise the linked list with a double node for a head
+  
+  public DoubleLinkedList(int a){
+    head = new DoubleNode(a);
   }
 }
